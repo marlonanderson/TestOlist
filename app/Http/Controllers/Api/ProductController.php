5da7028481491580eaf8016c3444dec129cc17fe
marlonanderson <marlonandersoncesar@gmail.com>
2021-02-04 17:18:29 -0300

    INICIO DO COMMIT

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Product;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function show (Product $id){
+        return $id;
+    }
+}
+
